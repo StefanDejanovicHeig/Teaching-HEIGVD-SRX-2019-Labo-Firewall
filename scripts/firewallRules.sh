2f4c8106902b6d25940cfe032133cdbd19982287
Lougranou <legrand.bruno@hotmail.com>
2019-03-26 20:09:29 +0100

    add flushfirewall.sh

--- scripts/firewallRules.sh
+++ scripts/firewallRules.sh
@@ -13,14 +13,47 @@ $docker iptables -P INPUT DROP
 $docker iptables -P OUTPUT DROP
 $docker iptables -P FORWARD DROP
 
+
+# return traffic 
+echo "0.2 Allows return traffic : RELATED, ESTABLISHED"
+$docker iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+$docker iptables -A FORWARD -m conntrack --ctstate INVALID -j DROP
+
+
+#---------
+# 2. PING
+#--------
+echo "2. Allows pings echo-request & echo-reply"
+#$docker iptables -A OUTPUT -p icmp --icmp-type 8 -j ACCEPT
+#$docker iptables -A INPUT -p icmp --icmp-type 0 -j ACCEPT
+
+# LAN --> WAN
+$docker iptables -A FORWARD -s 192.168.100.0/24 -i eth0 -p icmp --icmp-type 8 -j ACCEPT
+$docker iptables -A FORWARD -d 192.168.100.0/24 -p icmp --icmp-type 0 -j ACCEPT
+#$docker iptables -A FORWARD -d 192.168.100.0/24 -i eth2 -p icmp --icmp-type 0 -j ACCEPT
+
+# DMZ --> LAN
+$docker iptables -A FORWARD -s 192.168.200.0/24 -d 192.168.100.0/24 -p icmp --icmp-type 8 -j ACCEPT
+$docker iptables -A FORWARD -s 192.168.100.0/24 -d 192.168.200.0/24 -p icmp --icmp-type 0 -j ACCEPT
+
+# LAN --> DMZ
+$docker iptables -A FORWARD -s 192.168.100.0/24 -d 192.168.200.0/24 -p icmp --icmp-type 8 -j ACCEPT
+$docker iptables -A FORWARD -s 192.168.200.0/24 -d 192.168.100.0/24 -p icmp --icmp-type 0 -j ACCEPT
+
+echo "2. done"
+
+
+
 : '
 DEBUG
-
 iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
 
-'
+echo "0.1 Set default policy to 'DROP'"
+$docker iptables -P INPUT ACCEPT
+$docker iptables -P OUTPUT ACCEPT
+$docker iptables -P FORWARD ACCEPT
 
 
 
@@ -44,23 +77,6 @@ $docker iptables -A FORWARD -d 192.168.100.0/24 -p tcp --sport 53 -j ACCEPT
 
 
 
-#---------
-# 2. PING
-#--------
-echo "2. Allows pings echo-request & echo-reply"
-$docker iptables -A OUTPUT -p icmp --icmp-type 8 -j ACCEPT
-$docker iptables -A INPUT -p icmp --icmp-type 0 -j ACCEPT
-# LAN --> * (DMZ,LAN,...)
-$docker iptables -A FORWARD -s 192.168.100.0/24 -p icmp --icmp-type 8 -j ACCEPT
-$docker iptables -A FORWARD -d 192.168.100.0/24 -p icmp --icmp-type 0 -j ACCEPT
-# DMZ --> LAN
-$docker iptables -A FORWARD -s 192.168.200.0/24 -d 192.168.100.0/24 -p icmp --icmp-type 8 -j ACCEPT
-$docker iptables -A FORWARD -s 192.168.100.0/24 -d 192.168.200.0/24 -p icmp --icmp-type 0 -j ACCEPT
-
-# return traffic 
-echo "3.0 Allows return traffic : RELATED, ESTABLISHED"
-$docker iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-$docker iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
 
 #---------
 # 3. HTTP
@@ -113,5 +129,4 @@ echo "7. Allows to admin FIREWALL with SSH from LAN"
 # LAN --> FIREWALL
 $docker iptables -A INPUT -s 192.168.100.3 -p tcp --dport 22  -j ACCEPT
 $docker iptables -A OUTPUT -d 192.168.100.3 -p tcp  --dport 22 -j ACCEPT
-
-
+'
---
+++ scripts/flushfirewall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+docker="sudo docker exec -t firewall"
+$docker iptables -P INPUT ACCEPT
+$docker iptables -P OUTPUT ACCEPT
+$docker iptables -P FORWARD ACCEPT
+echo "0.0 flush iptable rules"
+$docker iptables -F
+$docker iptables -X
