b13810cd0bb3cdb88a55533aa9b52257d0fba5f0
Lougranou <legrand.bruno@hotmail.com>
2019-03-26 20:28:49 +0100

    DNS OK

--- scripts/firewallRules.sh
+++ scripts/firewallRules.sh
@@ -21,12 +21,9 @@ $docker iptables -A FORWARD -m conntrack --ctstate INVALID -j DROP
 
 
 #---------
-# 2. PING
+# 1. PING
 #--------
-echo "2. Allows pings echo-request & echo-reply"
-#$docker iptables -A OUTPUT -p icmp --icmp-type 8 -j ACCEPT
-#$docker iptables -A INPUT -p icmp --icmp-type 0 -j ACCEPT
-
+echo "1. Allows pings echo-request & echo-reply"
 # LAN --> WAN
 $docker iptables -A FORWARD -s 192.168.100.0/24 -i eth2 -p icmp --icmp-type 8 -j ACCEPT
 $docker iptables -A FORWARD -d 192.168.100.0/24 -i eth0 -p icmp --icmp-type 0 -j ACCEPT
@@ -39,93 +36,12 @@ $docker iptables -A FORWARD -s 192.168.100.0/24 -d 192.168.200.0/24 -p icmp --ic
 $docker iptables -A FORWARD -s 192.168.100.0/24 -d 192.168.200.0/24 -p icmp --icmp-type 8 -j ACCEPT
 $docker iptables -A FORWARD -s 192.168.200.0/24 -d 192.168.100.0/24 -p icmp --icmp-type 0 -j ACCEPT
 
-echo "2. done"
-
-
-
-: '
-DEBUG
-iptables -P INPUT ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -P FORWARD ACCEPT
-
-echo "0.1 Set default policy to 'DROP'"
-$docker iptables -P INPUT ACCEPT
-$docker iptables -P OUTPUT ACCEPT
-$docker iptables -P FORWARD ACCEPT
-
-
-
 #----------
-# 1. DNS
+# 2. DNS
 #----------
-echo "1. Allowing DNS lookups (tcp, udp port 53)" 
-# UDP
-$docker iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
-$docker iptables -A INPUT -p udp --sport 53 -j ACCEPT
-# LAN --> * (DMZ,LAN,...)
-$docker iptables -A FORWARD -s 192.168.100.0/24 -p udp --dport 53 -j ACCEPT
-$docker iptables -A FORWARD -d 192.168.100.0/24 -p udp --sport 53 -j ACCEPT
-
-# TCP
-$docker iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
-$docker iptables -A INPUT -p tcp --sport 53 -j ACCEPT
-# LAN --> * (DMZ,LAN,...)
-$docker iptables -A FORWARD -s 192.168.100.0/24 -p tcp --dport 53 -j ACCEPT
-$docker iptables -A FORWARD -d 192.168.100.0/24 -p tcp --sport 53 -j ACCEPT
+echo "2. Allows DNS lookups (tcp, udp port 53)" 
+# LAN --> WAN
+$docker iptables -A FORWARD -s 192.168.100.0/24 -i eth2 -p udp --dport 53 -j ACCEPT
+$docker iptables -A FORWARD -s 192.168.100.0/24 -i eth2 -p tcp --dport 53 -j ACCEPT
 
 
-
-
-#---------
-# 3. HTTP
-#--------
-echo "3. Allows HTTP Connection"
-
-
-# LAN --> * (DMZ,LAN,...)
-$docker iptables -A FORWARD -s 192.168.100.0/24 -p tcp --dport 80 -j ACCEPT
-$docker iptables -A FORWARD -d 192.168.100.0/24 -p tcp --sport 80 -j ACCEPT
-
-
-
-#---------
-# 4. HTTPS
-#--------
-echo "4. Allows HTTPS secure Connection"
-
-
-# LAN --> * (DMZ,LAN,...)
-$docker iptables -A FORWARD -s 192.168.100.0/24 -p tcp --dport 443 -j ACCEPT
-$docker iptables -A FORWARD -d 192.168.100.0/24 -p tcp --sport 443 -j ACCEPT
-
-
-#---------
-# 5. HTTP DMZ
-#--------
-echo "5. Allows to reach DMZ on 80 8080 from LAN and WAN"
-
-# (LAN, WAN, ...) * --> DMZ.3
-$docker iptables -A FORWARD -s 192.168.200.3 -p tcp --dport 80 -j ACCEPT
-$docker iptables -A FORWARD -d 192.168.200.3 -p tcp --sport 80 -j ACCEPT
-$docker iptables -A FORWARD -s 192.168.200.3 -p tcp --dport 8080 -j ACCEPT
-$docker iptables -A FORWARD -d 192.168.200.3 -p tcp --sport 8080 -j ACCEPT
-
-#---------
-# 6. SSH DMZ
-#--------
-echo "6. Allows to admin DMZ with SSH from LAN"
-
-# LAN --> DMZ.3
-$docker iptables -A FORWARD -s 192.168.100.3 -d 192.168.200.3 -p tcp --dport 22 -j ACCEPT
-$docker iptables -A FORWARD -s 192.168.200.3 -d 192.168.100.3 -p tcp --dport 22 -j ACCEPT
-
-#---------
-# 6. SSH FIREWALL
-#--------
-echo "7. Allows to admin FIREWALL with SSH from LAN"
-
-# LAN --> FIREWALL
-$docker iptables -A INPUT -s 192.168.100.3 -p tcp --dport 22  -j ACCEPT
-$docker iptables -A OUTPUT -d 192.168.100.3 -p tcp  --dport 22 -j ACCEPT
-'
